11c42d5335eae672f7141952b20299433a5bdcfc
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-28 16:00:28 -0600

    Create Iteration command line project

---
+++ Iteracion/Iteracion/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Iteracion
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/28/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

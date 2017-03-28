b4c5c9c6971e06e066d48943903e4cb031d7bf8f
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-28 16:13:29 -0600

    Iteration in Arrays, classic, fast and blocks

--- Iteracion/Iteracion/main.m
+++ Iteracion/Iteracion/main.m
@@ -10,8 +10,24 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        //NSArray
+        NSArray *list = @[@1, @2, @3];
+        
+        //Iterating classic way
+        for (int i = 0; i < list.count; i++) {
+            NSLog(@"El elemento %d vale %@", i, list[i]);
+        }
+        
+        //Fast iteration
+        for (id element in list) {
+            NSLog(@"EL elemento vale %@", element);
+        }
+        
+        //Blocks
+        [list enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSLog(@"El elemento %lu vale %@", idx, obj);
+        }];
     }
     return 0;
 }

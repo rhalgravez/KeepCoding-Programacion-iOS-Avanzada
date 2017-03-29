37a6917e0c7aad3fa3137de40fadae4fbae8ecd0
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-29 12:36:18 -0600

    Create a VeiwController and add to the xib file a UIImage and UIButton

---
+++ Gemelas/Gemelas/AGTImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTImageViewController.h
+//  Gemelas
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/29/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTImageViewController : UIViewController
+
+@end
---
+++ Gemelas/Gemelas/AGTImageViewController.m
@@ -0,0 +1,37 @@
+//
+//  AGTImageViewController.m
+//  Gemelas
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/29/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTImageViewController.h"
+
+@interface AGTImageViewController ()
+
+@end
+
+@implementation AGTImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

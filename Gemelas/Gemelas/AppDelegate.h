618c4bc60da62e962ee905d28ce14998e8d53083
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-29 12:25:01 -0600

    Create project "Gemelas"
    Project to see synchronous and asynchronous calls

---
+++ Gemelas/Gemelas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gemelas
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/29/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

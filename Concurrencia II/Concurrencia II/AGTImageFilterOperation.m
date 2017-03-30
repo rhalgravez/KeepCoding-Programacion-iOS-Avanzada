14611230a851a41e92cc254b4c34852e656f2c1a
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-30 16:03:37 -0600

    Update the view controller befor going to background and update the image with the new filter
    Also comments to know what we are going to do next, this code doesn't compile...yet

--- Concurrencia II/Concurrencia II/AGTImageFilterOperation.m
+++ Concurrencia II/Concurrencia II/AGTImageFilterOperation.m
@@ -25,4 +25,37 @@
     return self;
 }
 
+#pragma mark - Executing the Operation
+
+-(void)main {
+    //The filter is applied after the download so we need three things.
+    
+    //1) Update the viewController before going to background (show the activity indicator in the main thread)
+    [self performSelectorOnMainThread:@selector(updateViewControllerBeforBackground) withObject:nil waitUntilDone:NO];
+    
+    //2) Apply the coreImage filter in background
+    //2.1) Create context
+    //2.2) Create CIImage from the original image
+    //2.3) Create the filter
+    //2.4) Create the outputImage (the image with the filter)
+    //2.5) Create other filter (just for fun :D)
+    //2.6) Create the outputImage (the image with the filter, again because we aply two filters)
+    //2.7) Create the CGImage with the filters
+    
+    //3) Show the new image in main thread
+    [self performSelectorOnMainThread:@selector(updateViewControllerAfterBackgroundWithImage:) withObject:image waitUntilDone:NO];
+}
+
+#pragma mark - Utils
+-(void)updateViewControllerBeforBackground {
+    [self.imageViewController.activityView startAnimating];
+}
+
+-(void) updateViewControllerAfterBackgroundWithImage:(UIImage *)image {
+    [self.imageViewController.activityView stopAnimating];
+    self.imageViewController.ImageView.image = image;
+}
+
+
+
 @end

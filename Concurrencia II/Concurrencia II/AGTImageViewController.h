c694ada190254f13bd9ee891a07165e932fd26a8
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-30 13:17:43 -0600

    Add an imageView, activityIndicator and button to the view with theirs IBOutlets an IBAction

---
+++ Concurrencia II/Concurrencia II/AGTImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTImageViewController.h
+//  Concurrencia II
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/30/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTImageViewController : UIViewController
+
+@end

6d14d46fee561a01973d2cc319bb8b0af4604237
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-30 14:14:55 -0600

    Add viewController as the rootViewCOntroller to the appDelegate

--- Concurrencia II/Concurrencia II/AppDelegate.m
+++ Concurrencia II/Concurrencia II/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "AGTImageViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self.window = [UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    AGTImageViewController *imageVC = [[AGTImageViewController alloc] initWithNibName:nil bundle:nil];
+    self.window.rootViewController = imageVC;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 

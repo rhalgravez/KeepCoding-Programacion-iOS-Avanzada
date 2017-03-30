6aa98ab354c7c93385a2a14b46a21f038ec8348a
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-30 15:19:48 -0600

    Create new NSoperation subclass for the Core Image Filter

---
+++ Concurrencia II/Concurrencia II/AGTImageFilterOperation.h
@@ -0,0 +1,13 @@
+//
+//  AGTImageFilterOperation.h
+//  Concurrencia II
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/30/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTImageFilterOperation : NSOperation
+
+@end
---
+++ Concurrencia II/Concurrencia II/AGTImageFilterOperation.m
@@ -0,0 +1,13 @@
+//
+//  AGTImageFilterOperation.m
+//  Concurrencia II
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/30/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTImageFilterOperation.h"
+
+@implementation AGTImageFilterOperation
+
+@end

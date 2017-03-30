2a9c9c03a654b23a2435ec62ea7a5b03d02b8042
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-30 13:10:19 -0600

    Create project as an alternative solution to asynchronous calls with NSOperations

---
+++ Concurrencia II/Concurrencia II/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Concurrencia II
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/30/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

323d71fe57b59495112446f3fe0788e959691a54
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-30 13:40:23 -0600

    Create a subclass of NSOperation

---
+++ Concurrencia II/Concurrencia II/AGTImageDownloader.h
@@ -0,0 +1,13 @@
+//
+//  AGTImageDownloader.h
+//  Concurrencia II
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/30/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTImageDownloader : NSOperation
+
+@end
---
+++ Concurrencia II/Concurrencia II/AGTImageDownloader.m
@@ -0,0 +1,13 @@
+//
+//  AGTImageDownloader.m
+//  Concurrencia II
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/30/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTImageDownloader.h"
+
+@implementation AGTImageDownloader
+
+@end

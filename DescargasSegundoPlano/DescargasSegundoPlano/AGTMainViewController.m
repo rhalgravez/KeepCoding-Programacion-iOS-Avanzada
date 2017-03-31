82d305325e3e5e5435dd2f2b2559b827fdb01715
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-31 17:37:37 -0600

    Add url and URLSession properties
    Also. Fix..again the navigationBar overlaping

--- DescargasSegundoPlano/DescargasSegundoPlano/AGTMainViewController.m
+++ DescargasSegundoPlano/DescargasSegundoPlano/AGTMainViewController.m
@@ -14,9 +14,29 @@
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *ativityView;
 @property (weak, nonatomic) IBOutlet UIProgressView *progressIndicatorView;
 
+//URLs for the images
+@property (strong, nonatomic) NSURL *bulbasaurURL;
+@property (strong, nonatomic) NSURL *charmanderURL;
+
+//URLSession for normal download and background
+@property (strong, nonatomic) NSURLSession *downloadSession;
+@property (strong, nonatomic) NSURLSession *backgroundDonwloadSession;
+
 @end
 
 @implementation AGTMainViewController
+
+#pragma mark - Lifecycle
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    //Other way to fix the navigation bar overlapping the view
+    [self setEdgesForExtendedLayout:UIRectEdgeNone];
+    
+    //Set the URL with its image
+    self.bulbasaurURL = [NSURL URLWithString:@"http://vignette1.wikia.nocookie.net/pokemon/images/e/ea/001Bulbasaur_AG_anime.png"];
+    self.charmanderURL = [NSURL URLWithString:@"http://cartoonbros.com/wp-content/uploads/2016/11/Charmander-3.png"];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];

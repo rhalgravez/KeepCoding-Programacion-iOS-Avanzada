f5f337b3c395b2e4578a215110a27b3c282c92f4
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-31 15:43:15 -0600

    Add View Controlelr to the project
    Also disable autolayout and set the xib file fot ipad size

---
+++ DescargasSegundoPlano/DescargasSegundoPlano/AGTMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTMainViewController.h
+//  DescargasSegundoPlano
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/31/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTMainViewController : UIViewController
+
+@end

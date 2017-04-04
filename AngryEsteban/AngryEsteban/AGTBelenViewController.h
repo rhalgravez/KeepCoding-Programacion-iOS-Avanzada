179abf0d06b149c46ee6c97e299177c77681d0ef
Roberto Halgravez <roberto.halgravez@globant.com>
2017-04-04 16:25:10 -0500

    Add viewController to the project with an UIImage and its IBOutlet

---
+++ AngryEsteban/AngryEsteban/AGTBelenViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTBelenViewController.h
+//  AngryEsteban
+//
+//  Created by Roberto Manuel Halgravez Perea on 4/4/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTBelenViewController : UIViewController
+
+@end

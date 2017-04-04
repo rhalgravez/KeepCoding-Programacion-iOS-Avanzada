481f7b8e057f99073f7f1459298834f6f9b66f76
Roberto Halgravez <roberto.halgravez@globant.com>
2017-04-04 16:28:52 -0500

    Make the AGTBelenViewController the rootViewController

--- AngryEsteban/AngryEsteban/AppDelegate.m
+++ AngryEsteban/AngryEsteban/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "AGTBelenViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    AGTBelenViewController *belenVC = [[AGTBelenViewController alloc] init];
+    
+    self.window.rootViewController = belenVC;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 

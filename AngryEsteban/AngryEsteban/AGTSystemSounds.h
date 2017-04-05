086c03970e4052488f9055f29fddec64b5254c3e
Roberto Halgravez <roberto.halgravez@globant.com>
2017-04-05 16:04:09 -0500

    Add AGTSystemSounds files

---
+++ AngryEsteban/AngryEsteban/AGTSystemSounds.h
@@ -0,0 +1,13 @@
+//
+//  AGTSystemSounds.h
+//  AngryEsteban
+//
+//  Created by Roberto Manuel Halgravez Perea on 4/5/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTSystemSounds : NSObject
+
+@end
---
+++ AngryEsteban/AngryEsteban/AGTSystemSounds.m
@@ -0,0 +1,13 @@
+//
+//  AGTSystemSounds.m
+//  AngryEsteban
+//
+//  Created by Roberto Manuel Halgravez Perea on 4/5/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTSystemSounds.h"
+
+@implementation AGTSystemSounds
+
+@end

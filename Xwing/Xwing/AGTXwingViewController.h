9135569ca2e2da0abcb265858210701f8e5803fa
Roberto Halgravez <roberto.halgravez@globant.com>
2017-04-06 13:29:30 -0500

    Add a view controller and the resources

---
+++ Xwing/Xwing/AGTXwingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTXwingViewController.h
+//  Xwing
+//
+//  Created by Roberto Manuel Halgravez Perea on 4/6/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTXwingViewController : UIViewController
+
+@end

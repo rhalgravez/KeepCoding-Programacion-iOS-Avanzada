10225ea0c0f388b2063d4cb54002099d2334ce44
Roberto Halgravez <roberto.halgravez@globant.com>
2017-04-06 13:23:18 -0500

    Create new project called Xwing an animation example

---
+++ Xwing/Xwing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xwing
+//
+//  Created by Roberto Manuel Halgravez Perea on 4/6/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

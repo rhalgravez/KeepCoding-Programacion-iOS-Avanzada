a28ec1482e0610ebe8f0b99035f2755c1dcdae5d
Roberto Halgravez <roberto.halgravez@globant.com>
2017-04-10 12:02:35 -0500

    Create Everpobre porjet (a simple notes app)

---
+++ Everpobre/Everpobre/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Everpobre
+//
+//  Created by Roberto Manuel Halgravez Perea on 4/10/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

4bb0d8f5355525606d4d643ca5ee890a136e27db
Roberto Halgravez <roberto.halgravez@globant.com>
2017-04-10 15:52:54 -0500

    Create instance of AGTSimpleCoreDataStack in the AppDelegate so we can access the data base from any place in the prject

--- Everpobre/Everpobre/AppDelegate.h
+++ Everpobre/Everpobre/AppDelegate.h
@@ -8,9 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+@class AGTSimpleCoreDataStack;
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) AGTSimpleCoreDataStack *model;
 
 
 @end

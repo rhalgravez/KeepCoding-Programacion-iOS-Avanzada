ce887be53349c2ff78e3283140d3501dc4f06461
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-03 12:38:26 -0600

    Call the KVO methods in the NSManagedObject life cycle for AGTNote

--- Everpobre/Everpobre/AGTNote.m
+++ Everpobre/Everpobre/AGTNote.m
@@ -3,12 +3,17 @@
 @interface AGTNote ()
 
 // Private interface goes here.
++(NSArray *)observableKeyNames;
 
 @end
 
 @implementation AGTNote
 
 // Custom logic goes here.
+
++(NSArray *)observableKeyNames {
+    return @[@"name", @"creationDate", @"text", @"notebook", @"photo"];
+}
 
 +(instancetype)noteWithName:(NSString *)name
                    notebook:(AGTNotebook *)notebook
@@ -25,5 +30,52 @@
     
     return note;
 }
+
+#pragma mark - KVO
+
+-(void)setupKVO {
+    
+    for (NSString *key in [AGTNote observableKeyNames]) {
+        [self addObserver:self
+               forKeyPath:key
+                  options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
+                  context:NULL];
+    }
+    
+}
+
+-(void)tearDownKVO {
+    for (NSString *key in [AGTNote observableKeyNames]) {
+        [self removeObserver:self forKeyPath:key];
+    }
+    
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                      context:(void *)context {
+    
+    self.modificationDate = [NSDate date];
+    
+}
+
+#pragma mark - LifeCycle
+
+-(void)awakeFromInsert {
+    [super awakeFromInsert];
+    [self setupKVO];
+}
+
+-(void)awakeFromFetch {
+    [super awakeFromFetch];
+    [self setupKVO];
+}
+
+-(void)willTurnIntoFault {
+    [super willTurnIntoFault];
+    [self tearDownKVO];
+}
+
 
 @end

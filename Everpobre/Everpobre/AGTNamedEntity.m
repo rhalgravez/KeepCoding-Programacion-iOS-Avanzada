f460d8571f95e45568288d8e6bdb3e00976046b3
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-03 12:55:50 -0600

    Refactor the KVO methods and life cycle for the NSManagedObject

--- Everpobre/Everpobre/AGTNamedEntity.m
+++ Everpobre/Everpobre/AGTNamedEntity.m
@@ -3,11 +3,63 @@
 @interface AGTNamedEntity ()
 
 // Private interface goes here.
++(NSArray *)observableKeyNames;
 
 @end
 
 @implementation AGTNamedEntity
 
-// Custom logic goes here.
+#pragma mark - Class methods
+
++(NSArray *)observableKeyNames {
+    return @[@"name", @"creationDate"];
+}
+
+
+#pragma mark - KVO
+
+-(void)setupKVO {
+    
+    for (NSString *key in [[self class] observableKeyNames]) {
+        [self addObserver:self
+               forKeyPath:key
+                  options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
+                  context:NULL];
+    }
+    
+}
+
+-(void)tearDownKVO {
+    for (NSString *key in [[self class] observableKeyNames]) {
+        [self removeObserver:self forKeyPath:key];
+    }
+    
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                      context:(void *)context {
+    
+    self.modificationDate = [NSDate date];
+    
+}
+
+#pragma mark - LifeCycle
+
+-(void)awakeFromInsert {
+    [super awakeFromInsert];
+    [self setupKVO];
+}
+
+-(void)awakeFromFetch {
+    [super awakeFromFetch];
+    [self setupKVO];
+}
+
+-(void)willTurnIntoFault {
+    [super willTurnIntoFault];
+    [self tearDownKVO];
+}
 
 @end

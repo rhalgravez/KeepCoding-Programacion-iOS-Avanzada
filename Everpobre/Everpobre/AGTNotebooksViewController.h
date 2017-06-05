640986e11606c8c8537a25a55a4e26e5c546575f
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-05 16:01:08 -0600

    Add file for the notebooks tableview

---
+++ Everpobre/Everpobre/AGTNotebooksViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTNotebooksViewController.h
+//  Everpobre
+//
+//  Created by Roberto Halgravez on 6/5/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTCoreDataTableViewController.h"
+
+@interface AGTNotebooksViewController : AGTCoreDataTableViewController
+
+@end

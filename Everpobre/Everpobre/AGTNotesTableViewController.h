fc31e92edd72ef324681439add0fa0f40a102c67
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-09 13:48:42 -0600

    Add new files to show the NSPredicate results in a table view

---
+++ Everpobre/Everpobre/AGTNotesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTNotesTableViewController.h
+//  Everpobre
+//
+//  Created by Roberto Halgravez on 6/9/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTCoreDataTableViewController.h"
+
+@interface AGTNotesTableViewController : AGTCoreDataTableViewController
+
+@end

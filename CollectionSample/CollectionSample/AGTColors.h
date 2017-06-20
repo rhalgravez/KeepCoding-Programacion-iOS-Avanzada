b63264777da4ee8d1cd5aeba4fc1cf5d93b6ac78
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-20 14:34:26 -0600

    Create class AGTColors to get a random color

---
+++ CollectionSample/CollectionSample/AGTColors.h
@@ -0,0 +1,13 @@
+//
+//  AGTColors.h
+//  CollectionSample
+//
+//  Created by Roberto Halgravez on 6/20/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTColors : NSObject
+
+@end
---
+++ CollectionSample/CollectionSample/AGTColors.m
@@ -0,0 +1,13 @@
+//
+//  AGTColors.m
+//  CollectionSample
+//
+//  Created by Roberto Halgravez on 6/20/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import "AGTColors.h"
+
+@implementation AGTColors
+
+@end

e66c1ee886bac43da5ced7b9ae30cd0f66ae8529
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-20 14:54:17 -0600

    Add Controller class for the CollectionViewController

---
+++ CollectionSample/CollectionSample/AGTColorfulViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTColorfulViewController.h
+//  CollectionSample
+//
+//  Created by Roberto Halgravez on 6/20/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTColorfulViewController : UICollectionViewController
+
+@end
---
+++ CollectionSample/CollectionSample/AGTColorfulViewController.m
@@ -0,0 +1,22 @@
+//
+//  AGTColorfulViewController.m
+//  CollectionSample
+//
+//  Created by Roberto Halgravez on 6/20/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import "AGTColorfulViewController.h"
+
+@interface AGTColorfulViewController ()
+
+@end
+
+@implementation AGTColorfulViewController
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

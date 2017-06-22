88904a56655c345bcecb864e1c510d9c66f873f7
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-22 14:20:26 -0600

    Add the KVO methods for the cell

--- CollectionSample/CollectionSample/AGTRandomColorCell.m
+++ CollectionSample/CollectionSample/AGTRandomColorCell.m
@@ -7,7 +7,37 @@
 //
 
 #import "AGTRandomColorCell.h"
+#import "UIColor+Colorful.h"
 
 @implementation AGTRandomColorCell
+
+-(void)awakeFromNib {
+    [self setupKVO];
+}
+
+-(void) dealloc {
+    [self tearDownKVO];
+}
+
+#pragma mark - KVO
+-(void)setupKVO {
+    [self addObserver:self forKeyPath:@"color" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:NULL];
+}
+
+-(void)tearDownKVO {
+    [self removeObserver:self forKeyPath:@"color"];
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                      context:(void *)context {
+    self.hexView.text = [self.color hexString];
+    
+    [UIView animateWithDuration:0.6 animations:^{
+        self.backgroundColor = self.color;
+        self.hexView.textColor = [self.color contrastingTextColor];
+    }];
+}
 
 @end

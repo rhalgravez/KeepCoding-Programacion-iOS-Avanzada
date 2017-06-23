79923bbb0f6a19523ce9aa47d4a1637163d49a4b
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-22 18:18:18 -0600

    Add the headear and implementtion file for the previous nib fie we created, and set the IBOutlets for the elemtnes in the view

---
+++ Everpobre/Everpobre/AGTNoteCellView.h
@@ -0,0 +1,17 @@
+//
+//  AGTNoteCellView.h
+//  Everpobre
+//
+//  Created by Roberto Halgravez on 6/22/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTNoteCellView : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+@property (weak, nonatomic) IBOutlet UILabel *modificationDAteView;
+@property (weak, nonatomic) IBOutlet UILabel *titleView;
+
+@end
---
+++ Everpobre/Everpobre/AGTNoteCellView.m
@@ -0,0 +1,13 @@
+//
+//  AGTNoteCellView.m
+//  Everpobre
+//
+//  Created by Roberto Halgravez on 6/22/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTNoteCellView.h"
+
+@implementation AGTNoteCellView
+
+@end

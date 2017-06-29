5085658f17cc9ebd67607805ada635e5f4347e82
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-29 14:55:14 -0600

    Add protocol
    This protocol is for a refactor in all the future Details views that will have an initWithModel: method

---
+++ Everpobre/Everpobre/AGTDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  AGTDetailViewController.h
+//  Everpobre
+//
+//  Created by Roberto Halgravez on 6/29/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol AGTDetailViewController <NSObject>
+
+-(id)initWithModel:(id)model;
+
+@end

2c8899a26d2830697e70a8866f4f2ea563c4ff3f
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-06-29 11:20:02 -0600

    Add view controller for the Notes

---
+++ Everpobre/Everpobre/AGTNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTNoteViewController.h
+//  Everpobre
+//
+//  Created by Roberto Halgravez on 6/29/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTNoteViewController : UIViewController
+
+@end

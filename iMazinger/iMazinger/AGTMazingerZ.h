95842c894efd101421e0268a6bab3f0bfb2c4de6
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-06 13:10:22 -0600

    Add the file for my model (this is going to be the annotation Object that's why the class implements the protocol MKAnnotation)

---
+++ iMazinger/iMazinger/AGTMazingerZ.h
@@ -0,0 +1,14 @@
+//
+//  AGTMazingerZ.h
+//  iMazinger
+//
+//  Created by Roberto Halgravez on 7/6/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface AGTMazingerZ : NSObject <MKAnnotation>
+
+@end
---
+++ iMazinger/iMazinger/AGTMazingerZ.m
@@ -0,0 +1,16 @@
+//
+//  AGTMazingerZ.m
+//  iMazinger
+//
+//  Created by Roberto Halgravez on 7/6/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import "AGTMazingerZ.h"
+
+@implementation AGTMazingerZ
+
+#pragma mark - MKAnnotation
+
+
+@end

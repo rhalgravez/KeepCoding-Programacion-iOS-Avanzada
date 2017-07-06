f822b69768f3660835d1102a7ee2fdb400f88a49
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-05 20:45:23 -0600

    Add the code for the locationWithCLLocation:forNote:
    Check if the location already exist
    Also convert the latitude and longitude into an address

--- Everpobre/Everpobre/AGTLocation.h
+++ Everpobre/Everpobre/AGTLocation.h
@@ -1,5 +1,11 @@
 #import "_AGTLocation.h"
 
+@class AGTNote;
+
+@import CoreLocation;
+
 @interface AGTLocation : _AGTLocation
 // Custom logic goes here.
++(instancetype)locationWithCLLocation:(CLLocation*)location forNote:(AGTNote*)note;
+
 @end
--- Everpobre/Everpobre/AGTLocation.m
+++ Everpobre/Everpobre/AGTLocation.m
@@ -1,4 +1,7 @@
 #import "AGTLocation.h"
+#import "AGTNote.h"
+
+@import AddressBookUI;
 
 @interface AGTLocation ()
 
@@ -9,5 +12,42 @@
 @implementation AGTLocation
 
 // Custom logic goes here.
++(instancetype)locationWithCLLocation:(CLLocation*)location forNote:(AGTNote*)note {
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:[AGTLocation entityName]];
+    NSPredicate *latitude = [NSPredicate predicateWithFormat:@"latitude == %f", location.coordinate.latitude];
+    NSPredicate *longitude = [NSPredicate predicateWithFormat:@"longitude == %f", location.coordinate.longitude];
+    request.predicate = [NSCompoundPredicate andPredicateWithSubpredicates:@[latitude, longitude]];
+    
+    NSError * error = nil;
+    NSArray *results = [note.managedObjectContext executeFetchRequest:request error:&error];
+    
+    NSAssert(results, @"Error al buscar!");
+    
+    //Aprovechamos que ya existe esa location
+    if ([results count]) {
+        AGTLocation *found = [results lastObject];
+        [found addNotesObject:note];
+        return found;
+    } else {
+        AGTLocation *newLocation = [self insertInManagedObjectContext:note.managedObjectContext];
+        newLocation.latitudeValue = location.coordinate.latitude;
+        newLocation.longitudeValue = location.coordinate.longitude;
+        [newLocation addNotesObject:note];
+        
+        //Dirección
+        CLGeocoder *coder = [CLGeocoder new];
+        [coder reverseGeocodeLocation:location
+                    completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+                        if (error) {
+                            NSLog(@"Error with obtaining address!\n%@", error);
+                        } else {
+                            newLocation.address = [placemarks.lastObject.addressDictionary objectForKey:@"FormattedAddressLines"];
+                        }
+                    }];
+        
+        return newLocation;
+    }
+}
 
 @end

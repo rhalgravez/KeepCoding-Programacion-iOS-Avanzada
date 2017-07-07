f140ce7fc7bd3d583438cc7ffd9265ebf8918e85
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-06 23:18:00 -0600

    Had to add again the AGTmapSnapshot header file (there was a problem when we add the file the first tima)

--- Everpobre-Antes-de-MapKit/AGTMapSnapshot.h
+++ Everpobre-Antes-de-MapKit/AGTMapSnapshot.h
@@ -2,4 +2,9 @@
 
 @interface AGTMapSnapshot : _AGTMapSnapshot
 // Custom logic goes here.
+
+@property (nonatomic, strong) UIImage *image;
+
++(instancetype)mapSnapshotForLocation:(AGTLocation *)location;
+
 @end

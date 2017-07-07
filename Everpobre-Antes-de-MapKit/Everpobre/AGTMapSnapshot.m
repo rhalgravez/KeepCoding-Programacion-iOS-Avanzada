84a7da7e13b61e12e9224d1eb8330f00becad75e
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-06 23:05:46 -0600

    We have to create a KVO for the location property because when call insertInManagedObject in the class method the awakeFrominsert is called but we don't have the location yet, thats why we start observing the location so when the location is ready we can create the snapshot

--- Everpobre-Antes-de-MapKit/Everpobre/AGTMapSnapshot.m
+++ Everpobre-Antes-de-MapKit/Everpobre/AGTMapSnapshot.m
@@ -29,5 +29,62 @@
     
 }
 
+-(void)awakeFromInsert {
+    [super awakeFromInsert];
+    
+    [self startObserving];
+}
+
+-(void)awakeFromFetch {
+    [super awakeFromFetch];
+     
+    [self startObserving];
+}
+
+-(void)willTurnIntoFault {
+    [super willTurnIntoFault];
+    
+    [self stopObserving];
+}
+
+#pragma mark - KVO
+-(void)startObserving {
+    [self addObserver:self
+           forKeyPath:@"location"
+              options:NSKeyValueObservingOptionNew context:NULL];
+}
+
+-(void)stopObserving {
+    [self removeObserver:self forKeyPath:@"location"];
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                      context:(void *)context {
+    
+    //Recalculamos el mapSnapShot
+    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(self.location.latitudeValue, self.location.longitudeValue);
+    
+    MKMapSnapshotOptions *options = [MKMapSnapshotOptions new];
+    options.region = MKCoordinateRegionMakeWithDistance(center, 3, 3);
+    options.mapType = MKMapTypeHybrid;
+    options.size = CGSizeMake(150, 150);
+    
+    MKMapSnapshotter *shotter = [[MKMapSnapshotter alloc] initWithOptions:options];
+    
+    [shotter startWithCompletionHandler:^(MKMapSnapshot * _Nullable snapshot, NSError * _Nullable error) {
+        if (!error) {
+            //Sin error
+            self.image = snapshot.image;
+        } else {
+            //Uso este setPrimitiveLocation porque self.location = nil
+            //crearía un  bucle infinito y con el método de abajo accedo
+            //directaente a la variable de insatncia y no a la property
+            [self setPrimitiveLocation:nil];
+            [self.managedObjectContext deleteObject:self];
+        }
+    }];
+}
 
 @end
